a2518ee7589c19f30637dbd1042ff583b7655010
Jc Dior <jessicaclairdior@gmail.com>
2020-09-21 21:53:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,16 @@
 class Anagram
   attr_accessor :word
 
+@@new_array = []
+
 def initialize(word)
   @word = word
 end
 
-def match(word_array)
+def match(anagram_array)
+  if  anagram_array.each do |word|
+    else @@new_array << word
+  end
 end
 
 end
